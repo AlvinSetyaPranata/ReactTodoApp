07dad9a04e7ea2f969a59ba237b69607acbd9af0
AlvinSetyaPranata <comand.alvin@gmail.com>
2023-11-11 07:01:06 +0700

    Added HeroIcon to dependencies

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,15 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/outline"
 
 function App() {
   const navigate = useNavigate()
 
   const handleEdit = (id) => {
-
-    // sessionStorage.setItem("toEdit", JSON.stringify({
-    //   id: id,
-    // }))
-    navigate("/edit", {state: {id: id}})
+    navigate("/edit", { state: { id: id } })
   }
 
   return (
@@ -20,9 +17,14 @@ function App() {
 
       <h4 className="text-base md:text-xl font-semibold text-left mb-4">List of your todo:</h4>
 
-      <div className="grid gap-6 bg-slate-300 px-4 py-4 rounded-md">
+      <div className="grid gap-6 bg-slate-300 px-4 py-4 rounded-md hover:cursor-pointer relative">
+        <div className="bg-white rounded-full absolute -right-3">
+          <PencilSquareIcon className="w-[24px] h-[24px" />
+        </div>
+
+        {/* dynamic */}
         <div className="px-8 py-4 bg-gray-200 rounded-md">
-          <div onClick={() => handleEdit(0)} className="flex justify-between gap-20 hover:cursor-pointer">
+          <div onClick={() => handleEdit(0)} className="flex justify-between gap-20">
             <p>Go Swimming</p>
             <p>12.00</p>
           </div>
